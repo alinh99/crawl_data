ea4257d4f1591b9fff9cf77f61101331f815e548
alinh99 <alinh1803@gmail.com>
2022-03-16 12:54:07 +0700

    updated data

--- dictionary/spiders/words_list.py
+++ dictionary/spiders/words_list.py
@@ -12,20 +12,15 @@ class WordsListSpider(scrapy.Spider):
 
     def __init__(self):
         url_definition = 'https://www.dictionary4it.com/term/page/'
-        # url_words = 'https://www.dictionary4it.com/term/'
 
-        for page_definition in range(1, 64):
-            self.start_urls.append(url_definition + str(page_definition))
-
-        # for page_words in range(1, 64):
-        #     self.start_urls.append(url_definition + str(page_words))
+        for page in range(1, 65):
+            self.start_urls.append(url_definition + str(page))
 
     def parse(self, response):
 
-        print("procesing:"+response.url)
+        print("procesing:" + response.url)
         # Extract data using css selectors
-        # words = response.xpath(
-        #     "//ul/li[@class='category-item']/a/text()").get.strip()
+
         # words = response.css('li.category-item')
         for link in response.css('li.category-item a::attr(href)'):
             yield response.follow(link.get(), callback=self.parse_categories, priority=1)
@@ -34,24 +29,18 @@ class WordsListSpider(scrapy.Spider):
         #     yield {
         #         'word': word.css('a::text').get().strip(),
         #     }
-        #     NEXT_PAGE_SELECTOR = '.page-link + a::attr(href)'
-        #     next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-        #     if next_page:
-        #         yield scrapy.Request(
-        #             response.urljoin(next_page),
-        #             callback=self.parse)
 
     def parse_categories(self, response):
         definitions = response.css('div.dictionary__mean')
         explains = response.css('div.dictionary__explain')
-        # definitions.css('span::text').get().strip()
+
         if definitions:
             for definition in definitions:
-                yield{
-                        'definition': definition.css('span::text').get().strip()
-                    }
+                yield {
+                    'definition': definition.css('span::text').get()
+                }
         else:
             for explain in explains:
-                yield{
-                    'definition': ''
+                yield {
+                    'definition': explain.css('p::text').get()
                 }
